ae833a64ce2914146eb53a8fbd8767df46654ea1
Rob Lyon <you@rlyon.me>
2018-12-31 04:55:46 -0800

    Format and add hashicorp recipe
    Some linting and formating.  Removed the devel recipe and split into the vbox (virtualbox) and hashicorp recipies.  Added default attributes to define the user which if not specified is the SUDO_USER.

--- recipes/chrome.rb
+++ recipes/chrome.rb
@@ -1,13 +1,13 @@
 apt_repository 'google-chrome' do
-    uri 'http://dl.google.com/linux/chrome/deb/'
-    key 'https://dl.google.com/linux/linux_signing_key.pub'
-    distribution 'stable'
-    components ['main']
-    action :add
-    # Google will manage it after the install
-    not_if { ::File.exist?('/etc/apt/sources.list.d/google-chrome.list') }
+  uri 'http://dl.google.com/linux/chrome/deb/'
+  key 'https://dl.google.com/linux/linux_signing_key.pub'
+  distribution 'stable'
+  components ['main']
+  action :add
+  # Google will manage it after the install
+  not_if { ::File.exist?('/etc/apt/sources.list.d/google-chrome.list') }
 end
 
 package 'google-chrome-stable' do
-    action :install
+  action :install
 end
--- recipes/vscode.rb
+++ recipes/vscode.rb
@@ -17,18 +17,7 @@ end
 
 node['vscode']['extensions'].each do |ext|
   vscode_ext ext do
-    become_user 'rlyon'
+    become_user node['vogon']['user']
     action :install
   end
 end
-
-###
-### Commands run on the command line
-###
-# apt-get install curl
-# curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-# install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-# sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-# apt-get install apt-transport-https
-# apt-get update
-# apt-get install code
